ff74238e94acfe82489babbbae11fe463aaf2d85
T20210145 <2101297547@qq.com>
2021-10-02 15:29:57 +0800

    Default Changelist

--- src/com/haa/栈和队列/设计循环队列.java
+++ src/com/haa/栈和队列/设计循环队列.java
@@ -13,12 +13,12 @@ public class 设计循环队列 {
      */
     class MyCircularQueue {
 
-        private int[] data;
+        private final int[] data;
         private int head;
         private int tail;
         private int size;
 
-       // MyCircularQueue(k): 构造器，设置队列长度为 k 。
+        // MyCircularQueue(k): 构造器，设置队列长度为 k 。
         public MyCircularQueue(int k) {
             data = new int[k];
             head = -1;
@@ -26,63 +26,56 @@ public class 设计循环队列 {
             size = k;
         }
 
-       //  向循环队列插入一个元素。如果成功插入则返回真。
+        //  向循环队列插入一个元素。如果成功插入则返回真。
         public boolean enQueue(int value) {
             //队满不能插入
-            if (isFull() == true) {
+            if (isFull()) {
                 return false;
             }
-            //队空时要把head指针指向第一个
-            if (isEmpty() == true) {
+            //队列为空时要吧head指针指向第一个
+            if (isEmpty()) {
                 head = 0;
             }
-            //tail指针后移
             tail = (tail + 1) % size;
             data[tail] = value;
             return true;
         }
 
-       //从循环队列中删除一个元素。如果成功删除则返回真。
+        //从循环队列中删除一个元素。如果成功删除则返回真。
         public boolean deQueue() {
             //队空无法删除
-            if (isEmpty() == true) {
-                return false;
-            }
-            //特判只有一个元素的情况
+            if (isEmpty()) return false;
+            //只有一个元素需要特判
             if (head == tail) {
-                head = -1;
-                tail = -1;
-                return true;
+                head = tail = -1;
+            } else {
+                head = (head + 1) % size;
             }
-            head = (head + 1) % size;
             return true;
         }
 
-      // Front: 从队首获取元素。如果队列为空，返回 -1 。
+        // Front: 从队首获取元素。如果队列为空，返回 -1 。
         public int Front() {
-            if (isEmpty() == true) {
+            if (isEmpty()) {
                 return -1;
             }
             return data[head];
         }
 
-       // Rear: 获取队尾元素。如果队列为空，返回 -1 。
+        // Rear: 获取队尾元素。如果队列为空，返回 -1 。
         public int Rear() {
-            if (isEmpty() == true) {
-                return -1;
-            }
+            if (isEmpty()) return -1;
             return data[tail];
         }
 
-       //检查循环队列是否为空。
+        //检查循环队列是否为空。
         public boolean isEmpty() {
             return head == -1;
         }
 
-      //检查循环队列是否已满。
+        //检查循环队列是否已满。
         public boolean isFull() {
             return ((tail + 1) % size) == head;
         }
     }
-
 }
