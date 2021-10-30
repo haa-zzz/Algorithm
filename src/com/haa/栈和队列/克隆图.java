b9cbfbe6199024c5cb683425eda86d473748d8cf
T20210145 <2101297547@qq.com>
2021-10-30 12:36:50 +0800

    Default Changelist

--- src/com/haa/栈和队列/克隆图.java
+++ src/com/haa/栈和队列/克隆图.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class 克隆图 {
     /*
-    给你无向 连通 图中一个节点的引用，请你返回该图的 深拷贝（克隆）。
+    给你无向连通图中一个节点的引用，请你返回该图的深拷贝（克隆）。
 
     图中的每个节点都包含它的值 val（int） 和其邻居的列表（list[Node]）。
 
@@ -31,10 +31,10 @@ public class 克隆图 {
         注意：每个节点只能拷贝一次，我们需要记录当前节点是否被访问过，为此我们需要用到哈希表
         时间复杂度O(N)  空间复杂度O(N)
      */
-    private HashMap<Node,Node> visited = new HashMap<>();
+    HashMap<Node,Node> visited = new HashMap<>();
     public Node cloneGraph(Node node) {
         if(node==null){
-            return node;
+            return null;
         }
         //如果访问过，直接在哈希表中寻找对应节点
         if(visited.containsKey(node)){
@@ -44,8 +44,8 @@ public class 克隆图 {
         Node cloneNode = new Node(node.val,new ArrayList<>());
         visited.put(node,cloneNode);
         //dfs遍历
-        for(Node neighbor:node.neighbors){
-            cloneNode.neighbors.add(cloneGraph(neighbor));
+        for(Node neighbor : node.neighbors){
+            cloneNode.neighbors.add( cloneGraph(neighbor) );
         }
         return cloneNode;
     }
@@ -59,11 +59,10 @@ public class 克隆图 {
         HashMap<Node, Node> visited = new HashMap();
         Queue<Node> queue = new LinkedList<>();
         queue.offer(node);
-        //吧第一个节点克隆
+        //把第一个节点克隆
         visited.put(node,new Node(node.val,new ArrayList<>()));
         while (!queue.isEmpty()){
             Node head = queue.poll();
-
             for(Node neighbor : head.neighbors){
                 if (!visited.containsKey(neighbor)) {
                     // 如果没有被访问过，克隆并存储在哈希表中
@@ -74,7 +73,6 @@ public class 克隆图 {
                 // 更新当前节点的邻居列表
                 visited.get(head).neighbors.add(visited.get(neighbor));
             }
-
         }
         return visited.get(node);
     }
