6da669abb78031b2f6e1eb253c61b889c2d45a32
T20210145 <2101297547@qq.com>
2021-11-16 16:03:00 +0800

    Default Changelist

--- src/com/haa/二叉树/从先序与中序遍历序列构造二叉树.java
+++ src/com/haa/二叉树/从先序与中序遍历序列构造二叉树.java
@@ -16,7 +16,6 @@ public class 从先序与中序遍历序列构造二叉树 {
     方法：递归
         先序遍历第一个元素是根节点，通过该节点和中序遍历可以确定出左右孩子节点，从而构建二叉树，之后递归的去构建左右的子树
         注意：先序遍历是根-左-右，在递归时要先构建左支后构建右支
-
         为了更好的在中序遍历中确定根节点的位置，需要用到hashMap存储中序遍历的数组和下标
         时间复杂度O(n)
         空间复杂度O(n)
@@ -26,9 +25,9 @@ public class 从先序与中序遍历序列构造二叉树 {
         for(int i = 0; i < inorder.length; i++){
             map.put(inorder[i],i);
         }
-        return buildTreeHelper(preorder,0,preorder.length,inorder,0, map);
+        return buildTreeHelper(preorder,0,preorder.length,0, map);
     }
-    private TreeNode buildTreeHelper(int[] preorder, int p_start, int p_end, int[] inorder, int i_start,
+    private TreeNode buildTreeHelper(int[] preorder, int p_start, int p_end,  int i_start,
                                      HashMap<Integer, Integer> map){
         // preorder 为空，直接返回 null
         if(p_start == p_end){
@@ -42,12 +41,10 @@ public class 从先序与中序遍历序列构造二叉树 {
         int leftNum = i_root_index - i_start;
         //递归的构造左子树
         root.left = buildTreeHelper(preorder, p_start + 1, p_start + leftNum + 1,
-                inorder, i_start, map);
+                 i_start, map);
         //递归的构造右子树
         root.right = buildTreeHelper(preorder, p_start + leftNum + 1,
-                p_end, inorder, i_root_index + 1, map);
+                p_end, i_root_index + 1, map);
         return root;
     }
-
-
 }
