3e136d0c07a95f3c8d32ab566e431c900e579a60
21012 <21012@tencent.com>
2022-06-18 14:28:04 +0800

    新增autumnMovesMustWin

---
+++ src/autumnMovesMustWin/Main.kt
@@ -0,0 +1,13 @@
+package autumnMovesMustWin
+
+import kotlin.jvm.JvmStatic
+
+/**
+ * 执行函数
+ */
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        lengthOfLongestSubstring("abcabcbb")
+    }
+}
